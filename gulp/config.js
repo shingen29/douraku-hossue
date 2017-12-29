b2ea172af9996ef45ec573c25435acc1a828e381
Makoto Otsuka <mametra2580@gmail.com>
2017-12-29 17:24:47 +0900

    gulp 追加

---
+++ gulp/config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+
+var dest = './public'; // 出力先ディレクトリ
+var src = './public';  // ソースディレクトリ
+var relativeSrcPath = path.relative('.', src); // 後々、つかいます
+
+module.exports = {
+  src: src,
+  dest: dest,
+
+  // 各タスクごとの設定をこの下に追加していく
+  // jsのビルド設定
+/*
+  js: {
+    src: src + '/assets/js/**',
+    dest: dest + '/js',
+    uplify: true
+  },
+
+  // webpackの設定
+  webpack: {
+    entry: src + '/js/main.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['', '.js']
+    },
+  },
+*/
+
+  // stylus
+  stylus: {
+    src: [
+      src + '/assets/stylus/**/!(_)*'
+    ],
+    dest: dest + '/css/',
+    output: 'style.css',  // 出力ファイル名
+    autoprefixer: {
+      browsers: ['last 2 versions']
+    },
+    minify: false
+  },
+
+  // watch
+  watch: {
+    //js: relativeSrcPath + '/js/**',
+    styl: src + '/assets/stylus/**'
+    //jade: relativeSrcPath + '/www/**',
+    //www: relativeSrcPath + '/www/**'
+  },
+}
